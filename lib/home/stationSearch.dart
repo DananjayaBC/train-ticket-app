2c98b06f6402d6b21d50550e4e818f1a345d27a9
DananjayaBC <Dana.chanuka@gmail.com>
2021-02-24 11:30:31 +0530

    Complete the search

--- lib/home/stationSearch.dart
+++ lib/home/stationSearch.dart
@@ -1,11 +1,21 @@
-import 'package:autocomplete_textfield/autocomplete_textfield.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import 'package:train_ticket_app/models/stations.dart';
 import 'package:train_ticket_app/models/stations2.dart';
+
 import 'dart:convert';
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:convert/convert.dart';
 
 class StationSearch extends StatefulWidget {
+  final dio =
+      Dio(BaseOptions(baseUrl: 'http://api.lankagate.gov.lk:8280', headers: {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer 64b3fb8c-8619-3d2a-959f-6cc1f68839d1'
+  }));
   @override
   _StationSearchState createState() => _StationSearchState();
 }
@@ -23,7 +33,7 @@ class _StationSearchState extends State<StationSearch> {
   void getStations() async {
     try {
       final response = await http.get(
-          "https://onlinetrainticket-17091-default-rtdb.firebaseio.com/stationList.json");
+          "https://onlinetrainticket-17091-default-rtdb.firebaseio.com/train/stationList.json");
       if (response.statusCode == 200) {
         stations = loadStations(response.body);
         print('Stations: ${stations.length}');
@@ -39,7 +49,7 @@ class _StationSearchState extends State<StationSearch> {
   void getStations1() async {
     try {
       final response = await http.get(
-          "https://onlinetrainticket-17091-default-rtdb.firebaseio.com/stationList.json");
+          "https://onlinetrainticket-17091-default-rtdb.firebaseio.com/train/stationList.json");
       if (response.statusCode == 200) {
         stations1 = loadStations1(response.body);
         print('Stations: ${stations1.length}');
@@ -84,6 +94,13 @@ class _StationSearchState extends State<StationSearch> {
                 color: Colors.black,
               ),
             ),
+            Text(
+              stations.id.toString(),
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+            ),
           ],
         ),
       ),
@@ -105,155 +122,210 @@ class _StationSearchState extends State<StationSearch> {
                 color: Colors.black,
               ),
             ),
+            Text(
+              stations1.id1.toString(),
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
+  final _formKey = GlobalKey<FormState>();
+
+  var _autoValidate = false;
+  var _search;
+  var _search1;
+  List<dynamic> _directTrains;
+
+  void searchTrain(String query) async {
+    final response = await widget.dio.get(
+        "/railway/1.0/train/searchTrain?startStationID=$_search&endStationID=$_search1&startTime=00:00:00&endTime=23:59:00&lang=en&searchDate=2021-02-21");
+    setState(() {
+      _directTrains = response.data['RESULTS']['directTrains']['trainsList'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var myController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(245, 50, 111, 1.0),
-        title: Center(
-          child: Text("Buy Tickets"),
-        ),
+        title: Center(child: Text("Train Search")),
+        backgroundColor: Colors.red,
       ),
-      body: Container(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                loading
-                    ? CircularProgressIndicator()
-                    : searchTextField = AutoCompleteTextField<Stations>(
-                        key: key,
-                        clearOnSubmit: false,
-                        suggestions: stations,
-                        style: TextStyle(color: Colors.black, fontSize: 20.0),
-                        decoration: InputDecoration(
-                          contentPadding:
-                              EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)),
-                          hintText: 'I am At',
-                          hintStyle:
-                              TextStyle(color: Colors.grey.withOpacity(1.0)),
-                          focusColor: Colors.red,
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red[600]),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              autovalidate: _autoValidate,
+              child: Column(
+                children: [
+                  loading
+                      ? CircularProgressIndicator()
+                      : searchTextField = AutoCompleteTextField<Stations>(
+                          key: key,
+                          clearOnSubmit: false,
+                          suggestions: stations,
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                          decoration: InputDecoration(
+                            contentPadding:
+                                EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black)),
+                            hintText: 'I am At',
+                            hintStyle:
+                                TextStyle(color: Colors.grey.withOpacity(1.0)),
+                            focusColor: Colors.red,
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red[600]),
+                            ),
                           ),
-                        ),
-                        itemFilter: (item, query) {
-                          return item.name
-                              .toLowerCase()
-                              .startsWith(query.toLowerCase());
-                        },
-                        itemSorter: (a, b) {
-                          return a.name.compareTo(b.name);
-                        },
-                        itemSubmitted: (item) {
-                          setState(() {
+                          itemFilter: (item, query) {
+                            return item.name
+                                .toLowerCase()
+                                .startsWith(query.toLowerCase());
+                          },
+                          itemSorter: (a, b) {
+                            return a.name.compareTo(b.name);
+                          },
+                          itemSubmitted: (item) {
                             searchTextField.textField.controller.text =
                                 item.name;
-                          });
-                        },
-                        itemBuilder: (context, item) {
-                          return row(item);
-                        },
-                      ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    loading
-                        ? CircularProgressIndicator()
-                        : searchTextField1 = AutoCompleteTextField<Stations1>(
-                            key: key1,
-                            clearOnSubmit: false,
-                            suggestions: stations1,
-                            style:
-                                TextStyle(color: Colors.black, fontSize: 20.0),
-                            decoration: InputDecoration(
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
-                              enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.black)),
-                              hintText: 'I Want to Go',
-                              hintStyle: TextStyle(
-                                  color: Colors.grey.withOpacity(1.0)),
-                              focusColor: Colors.red,
-                              focusedBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(color: Colors.red[600]),
+                            _search = item.id.toString();
+                          },
+                          itemBuilder: (context, item) {
+                            return row(item);
+                          },
+                          controller: myController,
+                        ),
+                  // TextFormField(
+                  //   decoration: InputDecoration(
+                  //       prefix: Icon(Icons.search),
+                  //       hintText: 'I\'m at',
+                  //       border: OutlineInputBorder(),
+                  //       filled: true),
+                  //   onChanged: (value) {
+                  //     _search = value;
+                  //   },
+                  //   validator: (value) {
+                  //     if (value.isEmpty) {
+                  //       return 'Please enter a search term';
+                  //     }
+                  //     return null;
+                  //   },
+                  // ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      loading
+                          ? CircularProgressIndicator()
+                          : searchTextField1 = AutoCompleteTextField<Stations1>(
+                              key: key1,
+                              clearOnSubmit: false,
+                              suggestions: stations1,
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 20.0),
+                              decoration: InputDecoration(
+                                contentPadding:
+                                    EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 20.0),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.black)),
+                                hintText: 'I Want to Go',
+                                hintStyle: TextStyle(
+                                    color: Colors.grey.withOpacity(1.0)),
+                                focusColor: Colors.red,
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.red[600]),
+                                ),
                               ),
-                            ),
-                            itemFilter: (item1, query1) {
-                              return item1.name1
-                                  .toLowerCase()
-                                  .startsWith(query1.toLowerCase());
-                            },
-                            itemSorter: (c, d) {
-                              return c.name1.compareTo(d.name1);
-                            },
-                            itemSubmitted: (item1) {
-                              setState(() {
+                              itemFilter: (item1, query1) {
+                                return item1.name1
+                                    .toLowerCase()
+                                    .startsWith(query1.toLowerCase());
+                              },
+                              itemSorter: (c, d) {
+                                return c.name1.compareTo(d.name1);
+                              },
+                              itemSubmitted: (item1) {
                                 searchTextField1.textField.controller.text =
                                     item1.name1;
-                              });
-                            },
-                            itemBuilder: (context1, item1) {
-                              return row1(item1);
-                            },
-                          ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // InkWell(
-                    //   onTap: () {},
-                    //   child: Text(
-                    //     'SEARCH',
-                    //     style: TextStyle(
-                    //         color: Colors.red,
-                    //         fontSize: 20.0,
-                    //         fontWeight: FontWeight.bold),
-                    //   ),
-                    // ),
-                    // SizedBox(
-                    //   width: 38.0,
-                    // ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 16.0, horizontal: 34.0),
-                        decoration: BoxDecoration(
-                            color: Color.fromRGBO(245, 50, 111, 1.0),
-                            borderRadius: BorderRadius.circular(30.0)),
+                                _search1 = item1.id1.toString();
+                              },
+                              itemBuilder: (context1, item1) {
+                                return row1(item1);
+                              },
+                            ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: RawMaterialButton(
+                      onPressed: () {
+                        searchTrain(_search);
+                        searchTrain(_search1);
+                      },
+                      fillColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
                         child: Text(
-                          'SEARCH',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
+                          "Search",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
+            _directTrains == null
+                ? Expanded(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Colors.black12,
+                        size: 110,
+                      ),
+                      Text(
+                        'No results to display',
+                        style: TextStyle(
+                            color: Colors.black12,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ))
+                : Expanded(
+                    child: ListView(
+                      children: _directTrains.map((trainsList) {
+                        return ListTile(
+                          title: Text(trainsList['startStationName']),
+                          subtitle: Text(trainsList['endStationName']),
+                          trailing: Text(
+                              'Arivel Time-${trainsList['arrivalTimeFinalStation']}'),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+          ],
         ),
       ),
     );
